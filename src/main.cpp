39409ac6a2880ad815d6096231d0fcdcfd2547f6
Andrea Diamantini <adjam7@gmail.com>
2009-03-22 10:21:09 +0100

    Fixed Copyright intro

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 /* ============================================================
- *
- * This file is a part of the rekonq project
- *
- * Copyright (C) 2007-2008 Trolltech ASA. All rights reserved
- * Copyright (C) 2008 by Andrea Diamantini <adjam7 at gmail dot com>
- *
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * ============================================================ */
+*
+* This file is a part of the rekonq project
+*
+* Copyright (C) 2008 by Andrea Diamantini <adjam7 at gmail dot com>
+*
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
 
 
 #include "application.h"
@@ -28,7 +28,7 @@
 static const char description[] =
     I18N_NOOP("KDE Browser Webkit Based");
 
-static const char version[] = "0.0.4";
+static const char version[] = "0.0.5";
 
 int main(int argc, char **argv)
 {
