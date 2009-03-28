9e4dca9b627276d4558e9ec8a8475c136e7935f9
Andrea Diamantini <adjam7@gmail.com>
2009-03-28 15:48:24 +0100

    scripts

---
+++ scripts/Messages.sh
@@ -0,0 +1,5 @@
+# FIXME
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.cpp` -o $podir/rekonq.pot
+
---
+++ scripts/codingstyle.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# apply kdelibs coding style to all c, cpp and header files in src directory 
+# requirements: installed astyle 
+# 
+# The coding style is defined in http://techbase.kde.org/Policies/Kdelibs_Coding_Style 
+#
+# In rekonq we use some little different rules..
+
+{
+cd ..
+cd src
+
+astyle \
+--indent=spaces=4 \
+--brackets=break \
+--indent-labels \
+--pad=oper \
+--unpad=paren \
+--one-line=keep-statements \
+--convert-tabs \
+--indent-preprocessor \
+`find -type f -name '*.cpp'` `find -type f -name '*.h'`
+}
