4034b9e3ac34af00d43894e8dcabee736f9e2883
Andrea Diamantini <adjam7@gmail.com>
2009-06-30 02:20:22 +0200

    Rekonq 0.1.7 with trivial google search integrated in the urlbar && fake gg:// && wk:// protocol support

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ static const char description[] =
     I18N_NOOP("WebKit based Web Browser for KDE");
 
 
-static const char version[] = "0.1.6";
+static const char version[] = "0.1.7";
 
 
 int main(int argc, char **argv)
