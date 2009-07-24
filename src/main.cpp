3dd8b3dbee69aedb81136d956549eb9d568f9a71
Andrea Diamantini <adjam7@gmail.com>
2009-07-24 02:56:31 +0200

    rekonq 0.1.10 with last news. From now on, until rekonq 0.2 release jsut bugfixing and TODO related problems

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ static const char description[] =
     I18N_NOOP("WebKit based Web Browser for KDE");
 
 
-static const char version[] = "0.1.9";
+static const char version[] = "0.1.10";
 
 
 int main(int argc, char **argv)
