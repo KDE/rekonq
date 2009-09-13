001d3d08772e3836825cde58e5775009672607cd
Andrea Diamantini <adjam7@gmail.com>
2009-09-13 03:19:40 +0200

    rekonq 0.2.57 with new "new tab" page.
    I'm aware this is a "pre-alpha" feature, but I need this in master
    to let people play, comment, brainstorm this.. ;)

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ static const char description[] =
     I18N_NOOP("A lightweight Web Browser for KDE based on WebKit");
 
 
-static const char version[] = "0.2.56";
+static const char version[] = "0.2.57";
 
 
 int main(int argc, char **argv)
