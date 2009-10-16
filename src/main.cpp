7b7609e307ec9d2484c6b1987a8b598e6a951bee
Andrea Diamantini <adjam7@gmail.com>
2009-10-16 02:29:18 +0200

    rekonq 0.2.68 "All tabs" actions instead of "closed tabs" one. Fixes in the homepage and in the history chain

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ static const char description[] =
     I18N_NOOP("A lightweight Web Browser for KDE based on WebKit");
 
 
-static const char version[] = "0.2.67";
+static const char version[] = "0.2.68";
 
 
 int main(int argc, char **argv)
