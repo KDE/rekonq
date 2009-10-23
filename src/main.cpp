915735bd3ccb1cb432b71df56a01da8ce049fd68
Andrea Diamantini <adjam7@gmail.com>
2009-10-23 12:35:31 +0200

    rekonq 0.2.71 This seems good :D bugfixing until Monday alpha release..
    Here:
    - flickering link fix
    - some better code
    - external url opening and management fix
    - new startup options
    - (and obviously..) homepage fix

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ static const char description[] =
     I18N_NOOP("A lightweight Web Browser for KDE based on WebKit");
 
 
-static const char version[] = "0.2.70";
+static const char version[] = "0.2.71";
 
 
 int main(int argc, char **argv)
