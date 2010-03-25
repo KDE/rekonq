KDE/rekonq

commit 29e095b81d241a77f43bf4bf293d9428b6caea26

Add middle click support for next, back and home in the main toolbar

src/mainwindow.cpp
@@ -286,7 +286,8 @@ void MainWindow::setupActions()
     fullScreenShortcut.setAlternate( Qt::Key_F11 );
     a->setShortcut( fullScreenShortcut );
 
-    KStandardAction::home(this, SLOT(homePage()), actionCollection());
+    a = actionCollection()->addAction( KStandardAction::Home );
+    connect(a, SIGNAL(triggered(Qt::MouseButtons,Qt::KeyboardModifiers)), this, SLOT(homePage(Qt::MouseButtons)));
     KStandardAction::preferences(this, SLOT(preferences()), actionCollection());
 
     a = KStandardAction::redisplay(m_view, SLOT(webReload()), actionCollection());
@@ -345,14 +346,16 @@ void MainWindow::setupActions()
     connect(a, SIGNAL(triggered(bool)), this, SLOT(clearPrivateData()));
 
     // ========================= History related actions ==============================
-    a = KStandardAction::back(this, SLOT(openPrevious()) , actionCollection());
+    a = actionCollection()->addAction( KStandardAction::Back );
+    connect(a, SIGNAL(triggered(Qt::MouseButtons,Qt::KeyboardModifiers)), this, SLOT(openPrevious(Qt::MouseButtons)));
 
     m_historyBackMenu = new KMenu(this);
     a->setMenu(m_historyBackMenu);
     connect(m_historyBackMenu, SIGNAL(aboutToShow()), this, SLOT(aboutToShowBackMenu()));
     connect(m_historyBackMenu, SIGNAL(triggered(QAction *)), this, SLOT(openActionUrl(QAction *)));
 
-    KStandardAction::forward(this, SLOT(openNext()) , actionCollection());
+    a = actionCollection()->addAction( KStandardAction::Forward );
+    connect(a, SIGNAL(triggered(Qt::MouseButtons,Qt::KeyboardModifiers)), this, SLOT(openNext(Qt::MouseButtons)));
 
     // ============================== General Tab Actions ====================================
     a = new KAction(KIcon("tab-new"), i18n("New &Tab"), this);
@@ -929,9 +932,18 @@ void MainWindow::viewPageSource()
 }
 
 
+void MainWindow::homePage(Qt::MouseButtons btn)
+{
+    if(btn == Qt::MidButton)
+        Application::instance()->loadUrl( KUrl(ReKonfig::homePage()), Rekonq::SettingOpenTab );
+    else
+        currentTab()->view()->load( QUrl(ReKonfig::homePage()) );
+}
+
+
 void MainWindow::homePage()
 {
-    currentTab()->view()->load( QUrl(ReKonfig::homePage()) );
+    homePage(Qt::LeftButton);
 }
 
 
@@ -974,17 +986,41 @@ void MainWindow::browserLoading(bool v)
 
 void MainWindow::openPrevious()
 {
+    openPrevious(Qt::LeftButton);
+}
+
+
+void MainWindow::openPrevious(Qt::MouseButtons btn)
+{
     QWebHistory *history = currentTab()->view()->history();
     if (history->canGoBack())
-        history->goToItem(history->backItem());
+    {
+        KUrl back = history->backItem().url();
+        if(btn == Qt::MidButton)
+            Application::instance()->loadUrl(back, Rekonq::SettingOpenTab);
+        else
+            Application::instance()->loadUrl(back);
+    }
 }
 
 
 void MainWindow::openNext()
 {
+    openNext(Qt::LeftButton);
+}
+
+
+void MainWindow::openNext(Qt::MouseButtons btn)
+{
     QWebHistory *history = currentTab()->view()->history();
     if (history->canGoForward())
-        history->goToItem(history->forwardItem());
+    {
+        KUrl next = history->forwardItem().url();
+        if(btn == Qt::MidButton)
+            Application::instance()->loadUrl(next, Rekonq::SettingOpenTab);
+        else
+            Application::instance()->loadUrl(next);
+    }
 }
 
 

src/mainwindow.h
@@ -88,6 +88,7 @@ private:
     
 public slots:
     void homePage();
+    void homePage(Qt::MouseButtons);
 
     /**
      * Notifies a message in a popup
@@ -123,7 +124,9 @@ private slots:
 
     // history related
     void openPrevious();
+    void openPrevious(Qt::MouseButtons);
     void openNext();
+    void openNext(Qt::MouseButtons);
 
     // Find Action slots
     void find(const QString &);

commit fb54d4da487fe87636fb8be042df752702d8051b

Fixed a regression (history NOT modified) introduced with commit 29e095b81d2. Ehi guys, please take FULL attention, modifying working code..

src/mainwindow.cpp
@@ -1009,12 +1009,19 @@ void MainWindow::openPrevious(Qt::MouseButtons btn)
     QWebHistory *history = currentTab()->view()->history();
     if (history->canGoBack())
     {
-        KUrl back = history->backItem().url();
         if(btn == Qt::MidButton)
+        {
+            KUrl back = history->backItem().url();
             Application::instance()->loadUrl(back, Rekonq::SettingOpenTab);
+        }
         else
-            Application::instance()->loadUrl(back);
+        {
+            history->goToItem(history->backItem());
+        }
+        
+        updateActions();
     }
+    
 }
 
 
@@ -1023,11 +1030,16 @@ void MainWindow::openNext(Qt::MouseButtons btn)
     QWebHistory *history = currentTab()->view()->history();
     if (history->canGoForward())
     {
-        KUrl next = history->forwardItem().url();
         if(btn == Qt::MidButton)
+        {
+            KUrl next = history->forwardItem().url();
             Application::instance()->loadUrl(next, Rekonq::SettingOpenTab);
+        }
         else
-            Application::instance()->loadUrl(next);
+        {
+            history->goToItem(history->forwardItem());
+        }
+        updateActions();
     }
 }
 