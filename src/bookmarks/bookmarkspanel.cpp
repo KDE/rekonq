KDE/rekonq

commit 0d53995507a369d5b4cfddbe365fc8a438fcf3cf

New general panel class created and used by the bookmarks and history panel

src/bookmarks/bookmarkspanel.cpp
@@ -28,6 +28,9 @@
 // Self Includes
 #include "bookmarkspanel.h"
 
+// Auto Includes
+#include "rekonq.h"
+
 // Local Includes
 #include "panels/urlfilterproxymodel.h"
 #include "application.h"
@@ -37,27 +40,12 @@
 #include "bookmarkowner.h"
 #include "paneltreeview.h"
 
-// Auto Includes
-#include "rekonq.h"
-
-// Qt includes
-#include <QtGui/QHBoxLayout>
-#include <QtGui/QLabel>
-#include <QtGui/QHeaderView>
-
-// KDE includes
-#include <KLineEdit>
 
 BookmarksPanel::BookmarksPanel(const QString &title, QWidget *parent, Qt::WindowFlags flags)
-        : QDockWidget(title, parent, flags)
-        , m_treeView(new PanelTreeView(this))
+        : UrlPanel(title, parent, flags)
         , m_loadingState(false)
-        , m_loaded(false)
 {
     setObjectName("bookmarksPanel");
-    setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-
-    connect(this, SIGNAL(visibilityChanged(bool)), this, SLOT(showing(bool)));
 
     setVisible(ReKonfig::showBookmarksPanel());
 }
@@ -65,14 +53,7 @@ BookmarksPanel::BookmarksPanel(const QString &title, QWidget *parent, Qt::Window
 
 BookmarksPanel::~BookmarksPanel()
 {
-    ReKonfig::setShowBookmarksPanel(false);
-}
-
-
-void BookmarksPanel::showing(bool b)
-{
-    if(b && !m_loaded)
-        setup();
+    ReKonfig::setShowBookmarksPanel(!isHidden());
 }
 
 
@@ -96,6 +77,24 @@ void BookmarksPanel::contextMenu(const QPoint &pos)
                              );
 
     menu.exec(m_treeView->mapToGlobal(pos));
+}
+
+
+void BookmarksPanel::contextMenuItem(const QPoint &pos)
+{
+    contextMenu(pos);
+}
+
+
+void BookmarksPanel::contextMenuGroup(const QPoint &pos)
+{
+    contextMenu(pos);
+}
+
+
+void BookmarksPanel::contextMenuEmpty(const QPoint &pos)
+{
+    contextMenu(pos);
 }
 
 
@@ -131,12 +130,13 @@ void BookmarksPanel::onExpand(const QModelIndex &index)
 
 void BookmarksPanel::loadFoldedState(const QModelIndex &root)
 {
-    int count = m_treeView->model()->rowCount(root);
+    QAbstractItemModel *model = m_treeView->model();
+    int count = model->rowCount(root);
     QModelIndex index;
 
     for (int i = 0; i < count; ++i)
     {
-        index = m_treeView->model()->index(i, 0, root);
+        index = model->index(i, 0, root);
         if (index.isValid())
         {
             KBookmark bm = bookmarkForIndex(index);
@@ -152,57 +152,14 @@ void BookmarksPanel::loadFoldedState(const QModelIndex &root)
 
 void BookmarksPanel::setup()
 {
-    kDebug() << "Loading bookmarks panel setup...";
+    UrlPanel::setup();
+    kDebug() << "Bookmarks panel...";
 
-    QWidget *ui = new QWidget(this);
-
-    // setup search bar
-    QHBoxLayout *searchLayout = new QHBoxLayout;
-    searchLayout->setContentsMargins(5, 0, 0, 0);
-    QLabel *searchLabel = new QLabel(i18n("&Search:"));
-    searchLayout->addWidget(searchLabel);
-    KLineEdit *search = new KLineEdit;
-    search->setClearButtonShown(true);
-    searchLayout->addWidget(search);
-    searchLabel->setBuddy(search);
-
-    // setup tree view
-    m_treeView->setUniformRowHeights(true);
-    m_treeView->header()->hide();
-    m_treeView->setDragEnabled(true);
-    m_treeView->setAutoExpandDelay(750);
-    m_treeView->setDefaultDropAction(Qt::MoveAction);
-    m_treeView->viewport()->setAcceptDrops(true);
-
-    // put everything together
-    QVBoxLayout *vBoxLayout = new QVBoxLayout;
-    vBoxLayout->setContentsMargins(0, 0, 0, 0);
-    vBoxLayout->addLayout(searchLayout);
-    vBoxLayout->addWidget(m_treeView);
-
-    // add it to the UI
-    ui->setLayout(vBoxLayout);
-    setWidget(ui);
-
-    BookmarksTreeModel *model = new BookmarksTreeModel(this);
-    UrlFilterProxyModel *proxy = new UrlFilterProxyModel(ui);
-    proxy->setSourceModel(model);
-    m_treeView->setModel(proxy);
-
-    connect(search, SIGNAL(textChanged(const QString &)), proxy, SLOT(setFilterFixedString(const QString &)));
-
-    connect(model, SIGNAL(bookmarksUpdated()), this, SLOT(startLoadFoldedState()));
-
-    connect(m_treeView, SIGNAL(contextMenuItemRequested(const QPoint &)), this, SLOT(contextMenu(const QPoint &)));
-    connect(m_treeView, SIGNAL(contextMenuGroupRequested(const QPoint &)), this, SLOT(contextMenu(const QPoint &)));
-    connect(m_treeView, SIGNAL(contextMenuEmptyRequested(const QPoint &)), this, SLOT(contextMenu(const QPoint &)));
     connect(m_treeView, SIGNAL(delKeyPressed()), this, SLOT(deleteBookmark()));
     connect(m_treeView, SIGNAL(collapsed(const QModelIndex &)), this, SLOT(onCollapse(const QModelIndex &)));
     connect(m_treeView, SIGNAL(expanded(const QModelIndex &)), this, SLOT(onExpand(const QModelIndex &)));
 
     startLoadFoldedState();
-
-    m_loaded = true;
 }
 
 
@@ -216,4 +173,12 @@ KBookmark BookmarksPanel::bookmarkForIndex(const QModelIndex &index)
 
     BtmItem *node = static_cast<BtmItem*>(originalIndex.internalPointer());
     return node->getBkm();
+}
+
+
+QAbstractItemModel* BookmarksPanel::getModel()
+{
+    BookmarksTreeModel *model = new BookmarksTreeModel(this);
+    connect(model, SIGNAL(bookmarksUpdated()), this, SLOT(startLoadFoldedState()));
+    return model;
 }

src/CMakeLists.txt
@@ -33,6 +33,7 @@ SET( rekonq_KDEINIT_SRCS
     zoombar.cpp
     #----------------------------------------
     panels/urlfilterproxymodel.cpp
+    panels/urlpanel.cpp
     #----------------------------------------
     history/autosaver.cpp
     history/historymanager.cpp

src/bookmarks/bookmarkspanel.h
@@ -33,15 +33,14 @@
 // Rekonq Includes
 #include "rekonq_defines.h"
 
-// Qt Includes
-#include <QDockWidget>
+// Local Includes
+#include "panels/urlpanel.h"
 
 // Forward Declarations
-class PanelTreeView;
 class KBookmark;
 class QModelIndex;
 
-class REKONQ_TESTS_EXPORT BookmarksPanel : public QDockWidget
+class REKONQ_TESTS_EXPORT BookmarksPanel : public UrlPanel
 {
     Q_OBJECT
 
@@ -50,16 +49,16 @@ public:
     virtual ~BookmarksPanel();
 
 signals:
-    void openUrl(const KUrl &, const Rekonq::OpenType &);
-    void itemHovered(const QString &);
     void expansionChanged();
 
 public slots:
-    void showing(bool);
     void startLoadFoldedState();
 
 private slots:
     void contextMenu(const QPoint &pos);
+    virtual void contextMenuItem(const QPoint &pos);
+    virtual void contextMenuGroup(const QPoint &pos);
+    virtual void contextMenuEmpty(const QPoint &pos);
 
     void deleteBookmark();
     void onCollapse(const QModelIndex &index);
@@ -67,11 +66,12 @@ private slots:
     void loadFoldedState(const QModelIndex &root);
 
 private:
-    void setup();
+    virtual void setup();
     KBookmark bookmarkForIndex(const QModelIndex &index);
 
-    PanelTreeView *m_treeView;
-    bool m_loadingState, m_loaded;
+    virtual QAbstractItemModel* getModel();
+
+    bool m_loadingState;
 };
 
 #endif // BOOKMARKSPANEL_H

src/history/historypanel.cpp
@@ -33,105 +33,44 @@
 
 // Local Includes
 #include "panels/urlfilterproxymodel.h"
+
 #include "application.h"
 #include "paneltreeview.h"
 #include "historymodels.h"
 
-// Qt Includes
-#include <QtGui/QLabel>
-#include <QtGui/QHBoxLayout>
-#include <QtGui/QHeaderView>
-#include <QtGui/QTreeView>
-
-
 // KDE Includes
-#include <KLineEdit>
 #include <KLocalizedString>
 #include <KMenu>
 #include <KAction>
 #include <KMessageBox>
 
+// Qt Includes
+#include <QtGui/QHeaderView>
+
 
 HistoryPanel::HistoryPanel(const QString &title, QWidget *parent, Qt::WindowFlags flags)
-        : QDockWidget(title, parent, flags)
-        , m_treeView(new PanelTreeView(this))
-        , _loaded(false)
+        : UrlPanel(title, parent, flags)
 {
     setObjectName("historyPanel");
-    setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-
-    connect(this, SIGNAL(visibilityChanged(bool)), this, SLOT(showing(bool)));
-
-    setShown(ReKonfig::showHistoryPanel());
+    setVisible(ReKonfig::showHistoryPanel());
 }
 
 
 HistoryPanel::~HistoryPanel()
 {
-    // Save side panel's state
     ReKonfig::setShowHistoryPanel(!isHidden());
-}
-
-
-
-void HistoryPanel::showing(bool b)
-{
-    if(b && !_loaded)
-        setup();
 }
 
 
 void HistoryPanel::setup()
 {
-    kDebug() << "Loading history panel setup...";
+    UrlPanel::setup();
+    kDebug() << "History panel...";
 
-    QWidget *ui = new QWidget(this);
-
-    m_treeView->setUniformRowHeights(true);
-    m_treeView->setSelectionBehavior(QAbstractItemView::SelectRows);
-    m_treeView->setTextElideMode(Qt::ElideMiddle);
-    m_treeView->setAlternatingRowColors(true);
-    m_treeView->header()->hide();
-
-    // add search bar
-    QHBoxLayout *hBoxLayout = new QHBoxLayout;
-    hBoxLayout->setContentsMargins(5, 0, 0, 0);
-    QLabel *searchLabel = new QLabel(i18n("Search:"));
-    hBoxLayout->addWidget(searchLabel);
-    KLineEdit *search = new KLineEdit;
-    search->setClearButtonShown(true);
-    hBoxLayout->addWidget(search);
-    QWidget *searchBar = new QWidget;
-    searchBar->setLayout(hBoxLayout);
-
-    // setup layout
-    QVBoxLayout *vBoxLayout = new QVBoxLayout;
-    vBoxLayout->setContentsMargins(0, 0, 0, 0);
-    vBoxLayout->addWidget(searchBar);
-    vBoxLayout->addWidget(m_treeView);
-
-    // add it to the UI
-    ui->setLayout(vBoxLayout);
-    setWidget(ui);
-
-    //-
-    HistoryManager *historyManager = Application::historyManager();
-    QAbstractItemModel *model = historyManager->historyTreeModel();
-
-    UrlFilterProxyModel *proxy = new UrlFilterProxyModel(this);
-    proxy->setSourceModel(model);
-    m_treeView->setModel(proxy);
-    m_treeView->setExpanded(proxy->index(0, 0), true);
     m_treeView->header()->hideSection(1);
-    QFontMetrics fm(font());
-    int header = fm.width( QL1C('m') ) * 40;
-    m_treeView->header()->resizeSection(0, header);
 
-    connect(search, SIGNAL(textChanged(QString)), proxy, SLOT(setFilterFixedString(QString)));
-    connect(m_treeView, SIGNAL(contextMenuItemRequested(const QPoint &)), this, SLOT(contextMenuItem(const QPoint &)));
-    connect(m_treeView, SIGNAL(contextMenuGroupRequested(const QPoint &)), this, SLOT(contextMenuGroup(const QPoint &)));
-
-    _loaded = true;
+    const UrlFilterProxyModel *proxy = static_cast<const UrlFilterProxyModel*>(m_treeView->model());
+    m_treeView->expand(proxy->index(0, 0));
 }
 
 
@@ -173,6 +112,11 @@ void HistoryPanel::contextMenuGroup(const QPoint &pos)
 }
 
 
+void HistoryPanel::contextMenuEmpty(const QPoint& /*pos*/)
+{
+}
+
+
 void HistoryPanel::openAll()
 {
     QModelIndex index = m_treeView->currentIndex();
@@ -197,5 +141,11 @@ void HistoryPanel::openAll()
 
     for (int i = 0; i < allChild.length(); i++)
         emit openUrl(allChild.at(i).url(), Rekonq::NewTab);
+}
+
+
+QAbstractItemModel* HistoryPanel::getModel()
+{
+    return Application::historyManager()->historyTreeModel();
 }
 

src/panels/urlpanel.cpp
@@ -1,0 +1,115 @@
+/* ============================================================
+*
+* This file is a part of the rekonq project
+*
+* Copyright (C) 2009 by Domrachev Alexandr <alexandr.domrachev@gmail.com>
+* Copyright (C) 2009-2010 by Andrea Diamantini <adjam7 at gmail dot com>
+*
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License or (at your option) version 3 or any later version
+* accepted by the membership of KDE e.V. (or its successor approved
+* by the membership of KDE e.V.), which shall act as a proxy
+* defined in Section 14 of version 3 of the license.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ============================================================ */
+
+
+// Self Includes
+#include "urlpanel.h"
+
+// Auto Includes
+#include "rekonq.h"
+
+// Local Includes
+#include "panels/urlfilterproxymodel.h"
+
+#include "paneltreeview.h"
+
+// KDE Includes
+#include <KLineEdit>
+#include <KLocalizedString>
+
+// Qt Includes
+#include <QtGui/QLabel>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+
+
+UrlPanel::UrlPanel(const QString &title, QWidget *parent, Qt::WindowFlags flags)
+        : QDockWidget(title, parent, flags)
+        , m_treeView(new PanelTreeView(this))
+        , _loaded(false)
+{
+    setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+
+    connect(this, SIGNAL(visibilityChanged(bool)), this, SLOT(showing(bool)));
+}
+
+
+UrlPanel::~UrlPanel()
+{
+}
+
+
+void UrlPanel::showing(bool b)
+{
+    if(b && !_loaded)
+    {
+        setup();
+        _loaded = true;
+    }
+}
+
+
+void UrlPanel::setup()
+{
+    kDebug() << "Loading panel setup...";
+
+    QWidget *ui = new QWidget(this);
+
+    // setup search bar
+    QHBoxLayout *searchLayout = new QHBoxLayout;
+    searchLayout->setContentsMargins(5, 0, 0, 0);
+    QLabel *searchLabel = new QLabel(i18n("&Search:"));
+    searchLayout->addWidget(searchLabel);
+    KLineEdit *search = new KLineEdit;
+    search->setClearButtonShown(true);
+    searchLayout->addWidget(search);
+    searchLabel->setBuddy(search);
+
+    // setup tree view
+    m_treeView->setUniformRowHeights(true);
+    m_treeView->header()->hide();
+
+    // put everything together
+    QVBoxLayout *vBoxLayout = new QVBoxLayout;
+    vBoxLayout->setContentsMargins(0, 0, 0, 0);
+    vBoxLayout->addLayout(searchLayout);
+    vBoxLayout->addWidget(m_treeView);
+
+    // add it to the UI
+    ui->setLayout(vBoxLayout);
+    setWidget(ui);
+
+    QAbstractItemModel *model = getModel();
+    UrlFilterProxyModel *proxy = new UrlFilterProxyModel(this);
+    proxy->setSourceModel(model);
+    m_treeView->setModel(proxy);
+
+    connect(search, SIGNAL(textChanged(QString)), proxy, SLOT(setFilterFixedString(QString)));
+
+    connect(m_treeView, SIGNAL(contextMenuItemRequested(const QPoint &)), this, SLOT(contextMenuItem(const QPoint &)));
+    connect(m_treeView, SIGNAL(contextMenuGroupRequested(const QPoint &)), this, SLOT(contextMenuGroup(const QPoint &)));
+    connect(m_treeView, SIGNAL(contextMenuEmptyRequested(const QPoint &)), this, SLOT(contextMenuEmpty(const QPoint &)));
+}

src/panels/urlpanel.h
@@ -25,36 +25,52 @@
 * ============================================================ */
 
 
-#ifndef HISTORYPANEL_H
-#define HISTORYPANEL_H
+#ifndef URLPANEL_H
+#define URLPANEL_H
 
 
 // Rekonq Includes
 #include "rekonq_defines.h"
 
-// Local Includes
-#include "panels/urlpanel.h"
+// Qt Includes
+#include <QDockWidget>
+
+// Forward Declarations
+class PanelTreeView;
+
+class QAbstractItemModel;
 
 
-class REKONQ_TESTS_EXPORT HistoryPanel : public UrlPanel
+class REKONQ_TESTS_EXPORT UrlPanel : public QDockWidget
 {
     Q_OBJECT
 
 public:
-    explicit HistoryPanel(const QString &title, QWidget *parent = 0, Qt::WindowFlags flags = 0);
-    virtual ~HistoryPanel();
+    explicit UrlPanel(const QString &title, QWidget *parent = 0, Qt::WindowFlags flags = 0);
+    virtual ~UrlPanel();
 
-private slots:
-    virtual void contextMenuItem(const QPoint &pos);
-    virtual void contextMenuGroup(const QPoint &pos);
-    virtual void contextMenuEmpty(const QPoint &pos);
+public slots:
+    void showing(bool);
 
-    void openAll();
+signals:
+    void openUrl(const KUrl &, const Rekonq::OpenType &);
+    void itemHovered(const QString &);
 
-private:
+protected slots:
+    virtual void contextMenuItem(const QPoint &pos) = 0;
+    virtual void contextMenuGroup(const QPoint &pos) = 0;
+    virtual void contextMenuEmpty(const QPoint &pos) = 0;
+
+protected:
     virtual void setup();
 
-    virtual QAbstractItemModel* getModel();
+    virtual QAbstractItemModel* getModel() = 0;
+
+    PanelTreeView *m_treeView;
+
+private:
+    bool _loaded;
 };
 
-#endif // HISTORYPANEL_H
+
+#endif // URLPANEL_H

commit 25b74379011df9f33d7be078ff921a8842a04215

Fix the BK panel drag and drop regression introduced by 0d53995507a369d5b4cfddbe365fc8a438fcf3cf

src/bookmarks/bookmarkspanel.cpp
@@ -49,6 +49,8 @@ BookmarksPanel::BookmarksPanel(const QString &title, QWidget *parent, Qt::Window
 {
     setObjectName("bookmarksPanel");
     setVisible(ReKonfig::showBookmarksPanel());
+    panelTreeView()->setDragEnabled(true);
+    panelTreeView()->setAcceptDrops(true);
     connect(_bkTreeModel, SIGNAL(bookmarksUpdated()), this, SLOT(startLoadFoldedState()));
 }
 