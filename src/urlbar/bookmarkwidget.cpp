ef0c5630983853473e1e3c97a4c68baa1d487e6e
Jon Ander Peñalba <jonan88@gmail.com>
2010-09-10 12:50:00 +0200

    Fixes a string change (this commit should be reverted after v0.6)

--- src/urlbar/bookmarkwidget.cpp
+++ src/urlbar/bookmarkwidget.cpp
@@ -64,7 +64,10 @@ BookmarkWidget::BookmarkWidget(const KBookmark &bookmark, QWidget *parent)
     // Title
     QVBoxLayout *vLayout = new QVBoxLayout(this);
     QLabel *bookmarkInfo = new QLabel(this);
-    bookmarkInfo->setText(i18n("<h4>Edit this Bookmark</h4>"));
+    bookmarkInfo->setText(i18n("Edit this Bookmark"));
+    QFont font;
+    font.setPointSize(font.pointSize() + 2);
+    bookmarkInfo->setFont(font);
     bookmarkInfo->setAlignment(Qt::AlignCenter);
     vLayout->addWidget(bookmarkInfo);
 
