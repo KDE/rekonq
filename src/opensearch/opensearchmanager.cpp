KDE/rekonq

commit 3dbfc06247db3c62a73f1f3022b648197ae24a00

Krazy Fixes!! Add #rekonq_define.h to files that now use QL1C

src/urlbar/webshortcutwidget.cpp
@@ -18,6 +18,7 @@
  * Boston, MA 02110-1301, USA.
  */
 
+#include "rekonq_defines.h"
 #include "webshortcutwidget.h"
 
 #include <QtCore/QTimer>
@@ -138,11 +139,11 @@ void WebShortcutWidget::cancelClicked()
 void WebShortcutWidget::shortcutsChanged(const QString& newShorthands)
 {
     int savedCursorPosition = m_wsLineEdit->cursorPosition();
-    QString normalizedShorthands = QString(newShorthands).replace(" ", ",");
+    QString normalizedShorthands = QString(newShorthands).replace(QL1C(' '), QL1C(','));
     m_wsLineEdit->setText(normalizedShorthands);
     m_wsLineEdit->setCursorPosition(savedCursorPosition);
 
-    QSet<QString> shorthands = normalizedShorthands.split(",").toSet();
+    QSet<QString> shorthands = normalizedShorthands.split(QL1C(',')).toSet();
     QString contenderName = "";
     QString contenderWS = "";
 

src/webtab.cpp
@@ -26,6 +26,7 @@
 
 
 // Self Includes
+#include "rekonq_defines.h"
 #include "webtab.h"
 #include "webtab.moc"
 // Auto Includes
@@ -254,7 +255,7 @@ KUrl WebTab::extractOpensearchUrl(QWebElement e)
         QString host = docUrl.scheme() + "://" + docUrl.host();
         if (docUrl.port() != -1)
         {
-            host += ":" + QString::number(docUrl.port());
+            host += QL1C(':') + QString::number(docUrl.port());
         }
         url = KUrl(docUrl, href);
     }

src/opensearch/opensearchengine.cpp
@@ -58,7 +58,7 @@ QString OpenSearchEngine::parseTemplate(const QString &searchTerm, const QString
     // Simple conversion to RFC 3066.
     language = language.replace(QL1C('_'), QL1C('-'));
     QString country = language;
-    country = (country.replace(0, country.indexOf("-")+1, "")).toLower();
+    country = (country.remove(0, country.indexOf(QL1C('-'))+1)).toLower();
     const int firstDashPosition = country.indexOf(QL1C('-'));
     if (firstDashPosition >= 0)
         country = country.mid(firstDashPosition+1);

commit 4311a749ecab72bef935958a32a3caaf60475711

Fixes as pointed out by Krazy  * Fix duplicate includes  * Include own header first ( caused by 3dbfc06247db3c62a73f1f3022b648197ae24a00 )

src/opensearch/opensearchmanager.cpp
@@ -43,7 +43,6 @@
 #include <KUrl>
 #include <kio/scheduler.h>
 #include <KService>
-#include <KStandardDirs>
 #include <KDE/KMessageBox>
 #include <KUriFilterData>
 #include <KConfigGroup>

src/urlbar/webshortcutwidget.cpp
@@ -18,8 +18,8 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#include "rekonq_defines.h"
 #include "webshortcutwidget.h"
+#include "rekonq_defines.h"
 
 #include <QtCore/QTimer>
 #include <QtCore/QSet>

src/webtab.cpp
@@ -26,8 +26,8 @@
 
 
 // Self Includes
-#include "rekonq_defines.h"
 #include "webtab.h"
+#include "rekonq_defines.h"
 #include "webtab.moc"
 // Auto Includes
 #include "rekonq.h"