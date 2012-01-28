KDE/rekonq

commit 5b07ae284dc0179f6d1259072c20a24dd6048ba3

Renamed variable to avoid confusion - it is not a member variable

src/sessionmanager.cpp
@@ -44,9 +44,9 @@
 #include <QtCore/QFile>
 
 // Only used internally
-bool readSessionDocument(QDomDocument & document, const QString & m_sessionFilePath)
+bool readSessionDocument(QDomDocument & document, const QString & sessionFilePath)
 {
-    QFile sessionFile(m_sessionFilePath);
+    QFile sessionFile(sessionFilePath);
 
     if (!sessionFile.exists())
         return false;

commit 8a83079d858984c4075c9f1a5ecff2d0a83b7e5a

Always Create New Tabs
Commit 5b07ae28 introduced a bug here because no tab is now created by
default in the restoring window
BUG: 292586

src/sessionmanager.cpp
@@ -287,7 +287,7 @@ bool SessionManager::restoreMainWindow(MainWindow* window)
             if (tab.hasAttribute("currentTab"))
                 currentTab = tabNo;
 
-            WebView *view = (tabNo == 0) ? mv->webTab(0)->view() : mv->newWebTab()->view();
+            WebView *view = mv->newWebTab()->view();
 
             QDomCDATASection historySection = tab.firstChild().toCDATASection();
             QByteArray history = QByteArray::fromBase64(historySection.data().toAscii());