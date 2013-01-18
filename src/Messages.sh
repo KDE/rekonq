1d48ae2a4f06f4f498f4c3999b26febe72c7771e
Burkhard Lück <lueck@hube-lueck.de>
2013-01-19 00:41:24 +0100

    use find instead of manually specifying files+subdirs (which breaks when they are moved/renamed) to make GUI fully translated again

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC *.ui */*ui *.rc >> rc.cpp || exit 1
-$XGETTEXT *.cpp */*.cpp -o $podir/rekonq.pot
+$EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name '*.cpp'` -o $podir/rekonq.pot
 rm -f rc.cpp
 
